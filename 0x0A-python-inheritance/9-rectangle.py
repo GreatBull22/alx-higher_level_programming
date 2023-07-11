4f54d4c27b56c4d9ceda4badca50beb5d0187336
GreatBull22 <brandonmpofu22@gmail.com>
2023-07-11 12:42:15 -0700

    0x0A. Python - Inheritance

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,28 +1,25 @@
 #!/usr/bin/python3
+""" Module of class BaseGeometry """
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
-"""
-===================================
-module with class BaseGeometry
-===================================
-"""
 
 
 class Rectangle(BaseGeometry):
-    """Rectangle class that inherits from BaseGeometry"""
+    """ Function contructor that instance the private attributes
+
+        Atrributes:
+            width (int): width
+            height (int): height
+    """
 
     def __init__(self, width, height):
-        """Method for initialized the attrubutes"""
         self.integer_validator("width", width)
-        self.__width = width
         self.integer_validator("height", height)
+        self.__width = width
         self.__height = height
 
     def area(self):
-        """Method to redefine a area method in the parent class"""
-
-        return self.__width * self.__height
+        return (self.__width * self.__height)
 
     def __str__(self):
-        """__str__ method for return the next string"""
-
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+        return ("[Rectangle] {:d}/{:d}".format(self.__width, self.__height))
